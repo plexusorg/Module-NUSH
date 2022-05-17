1207990b7702810dacc679ed20cedff73aaa0e57
Luna <90072930+LunaWasFlaggedAgain@users.noreply.github.com>
2022-05-17 15:12:07 -0300

    Make everything work (#1)

--- src/main/java/dev/plex/nush/handler/impl/ListenerHandler.java
+++ src/main/java/dev/plex/nush/handler/impl/ListenerHandler.java
@@ -2,12 +2,14 @@ package dev.plex.nush.handler.impl;
 
 import dev.plex.nush.NushModule;
 import dev.plex.nush.handler.Handler;
+import dev.plex.nush.listener.impl.ChatListener;
 import dev.plex.nush.listener.impl.JoinListener;
 
 public class ListenerHandler implements Handler {
 
 	@Override
 	public void init(NushModule module) {
+		module.registerListener(new ChatListener());
 		module.registerListener(new JoinListener());
 	}
 }
