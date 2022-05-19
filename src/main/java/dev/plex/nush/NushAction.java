47a52ded3bc85ebc5da9351310897d562b3eb852
Telesphoreo <me@telesphoreo.me>
2022-05-19 18:57:20 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "https://media.discordapp.net/attachments/968519299763413022/976916680032800868/caption.gif""""""""""""""""
    This reverts commit f97cc0a44a000257bf280d0c5048808e80548ee9.

--- src/main/java/dev/plex/nush/NushAction.java
+++ src/main/java/dev/plex/nush/NushAction.java
@@ -2,29 +2,34 @@ package dev.plex.nush;
 
 import javax.annotation.Nullable;
 
-public enum NushAction {
-	MUTE("Mute Player", 0),
-	CANCEL("Cancel", 1),
-	SMITE("Smite", 2),
-	BAN("Ban Player", 3),
-	ACCEPT("Accept", 4);
+public enum NushAction
+{
+    MUTE("Mute Player", 0),
+    CANCEL("Cancel", 1),
+    SMITE("Smite", 2),
+    BAN("Ban Player", 3),
+    ACCEPT("Accept", 4);
 
-	public final String humanReadable;
-	public final int ordinal;
+    public final String humanReadable;
+    public final int ordinal;
 
-	NushAction(String humanReadable, int ordinal) {
-		this.humanReadable = humanReadable;
-		this.ordinal = ordinal;
-	}
+    NushAction(String humanReadable, int ordinal)
+    {
+        this.humanReadable = humanReadable;
+        this.ordinal = ordinal;
+    }
 
-	@Nullable
-	public static NushAction fromOrdinal(int ordinal) {
-		for (NushAction value : NushAction.values()) {
-			if (value.ordinal == ordinal) {
-				return value;
-			}
-		}
+    @Nullable
+    public static NushAction fromOrdinal(int ordinal)
+    {
+        for (NushAction value : NushAction.values())
+        {
+            if (value.ordinal == ordinal)
+            {
+                return value;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
