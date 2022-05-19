03e62d801709383ad1a4b8faefc39f1624c6991f
Allink <arclicious@vivaldi.net>
2022-05-19 20:57:09 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "https://media.discordapp.net/attachments/968519299763413022/976916680032800868/caption.gif"""""""""""""
    This reverts commit 4f3cfd182f3c5f4df91a5f057929c7932d097c17.

--- src/main/java/dev/plex/nush/listener/impl/JoinListener.java
+++ src/main/java/dev/plex/nush/listener/impl/JoinListener.java
@@ -10,25 +10,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public class JoinListener extends PlexListener
-{
+public class JoinListener extends PlexListener {
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
-        Player player = event.getPlayer();
-        NushModule module = NushModule.getInstance();
-        Plex plex = module.getPlex();
-        PlexPlayer plexPlayer = DataUtils.getPlayer(player.getUniqueId());
-        RankManager rankManager = plex.getRankManager();
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		NushModule module = NushModule.getInstance();
+		Plex plex = module.getPlex();
+		PlexPlayer plexPlayer = DataUtils.getPlayer(player.getUniqueId());
+		RankManager rankManager = plex.getRankManager();
 
-        if (!rankManager.isAdmin(plexPlayer))
-        {
-            return; // we only want to add admins
-        }
+		if (!rankManager.isAdmin(plexPlayer)) {
+			return; // we only want to add admins
+		}
         /*if (ChatListener.work.containsKey())
         {
 
         }*/
-    }
+	}
 }
