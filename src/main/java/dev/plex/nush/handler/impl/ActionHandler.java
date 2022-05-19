7fdd008ae7d12315b9ddb2074d3b49333c6debdf
Telesphoreo <me@telesphoreo.me>
2022-05-19 13:47:09 -0500

    Revert "Revert "Revert "Revert "https://media.discordapp.net/attachments/968519299763413022/976916680032800868/caption.gif""""
    This reverts commit 82d195d7753571eb3c5508a03fc9fc6d70274e42.

--- src/main/java/dev/plex/nush/handler/impl/ActionHandler.java
+++ src/main/java/dev/plex/nush/handler/impl/ActionHandler.java
@@ -12,7 +12,6 @@ import dev.plex.util.minimessage.SafeMiniMessage;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
@@ -24,58 +23,67 @@ import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public class ActionHandler implements Handler {
+public class ActionHandler implements Handler
+{
 
-	public static final Map<UUID, Message> MAP = new HashMap<>();
-	private final static TextReplacementConfig URL_REPLACEMENT_CONFIG = TextReplacementConfig.builder()
-		.match("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
-		.replacement(
-			(matchResult, builder) -> Component.empty().content(matchResult.group()).clickEvent(
-				ClickEvent.openUrl(matchResult.group()))).build();
-	private static NushModule MODULE;
+    public static final Map<UUID, Message> MAP = new HashMap<>();
+    private final static TextReplacementConfig URL_REPLACEMENT_CONFIG = TextReplacementConfig.builder()
+            .match("(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
+            .replacement(
+                    (matchResult, builder) -> Component.empty().content(matchResult.group()).clickEvent(
+                            ClickEvent.openUrl(matchResult.group()))).build();
+    private static NushModule MODULE;
 
-	public static void resolve(UUID uuid, NushAction action) {
-		Message message = MAP.get(uuid);
-		if (message == null) {
-			return;
-		}
-		if (action == NushAction.ACCEPT) {
-			for (Player player : Bukkit.getOnlinePlayers()) {
-				if (player.getUniqueId() != message.getSender()) {
-					player.sendMessage(Identity.identity(message.getSender()), getMessage(message));
-				}
-			}
-		}
-		MAP.remove(uuid);
-	}
+    public static void resolve(UUID uuid, NushAction action)
+    {
+        Message message = MAP.get(uuid);
+        if (message == null)
+        {
+            return;
+        }
+        if (action == NushAction.ACCEPT)
+        {
+            for (Player player : Bukkit.getOnlinePlayers())
+            {
+                if (player.getUniqueId() != message.getSender())
+                {
+                    player.sendMessage(Identity.identity(message.getSender()), getMessage(message));
+                }
+            }
+        }
+        MAP.remove(uuid);
+    }
 
-	public static Component getMessage(Message message) {
-		String text = PlexUtils.getTextFromComponent(message.getMessage());
-		Plex plex = MODULE.getPlex();
-		PlexPlayer plexPlayer = DataUtils.getPlayer(message.getSender());
-		Component prefix = plex.getRankManager().getPrefix(plexPlayer);
-		Component component = Component.empty();
+    public static Component getMessage(Message message)
+    {
+        String text = PlexUtils.getTextFromComponent(message.getMessage());
+        Plex plex = MODULE.getPlex();
+        PlexPlayer plexPlayer = DataUtils.getPlayer(message.getSender());
+        Component prefix = plex.getRankManager().getPrefix(plexPlayer);
+        Component component = Component.empty();
 
-		if (prefix != null) {
-			component = component.append(prefix);
-		}
+        if (prefix != null)
+        {
+            component = component.append(prefix);
+        }
 
-		return component.append(Component.space()).append(
-				PlexUtils.mmDeserialize(plex.config.getString("chat.name-color", "<white>") +
-					MiniMessage.builder().tags(
-						TagResolver.resolver(StandardTags.color(), StandardTags.rainbow(),
-							StandardTags.decorations(), StandardTags.gradient(),
-							StandardTags.transition()
-						)).build().serialize(plexPlayer.getPlayer().displayName())))
-			.append(Component.space())
-			.append(Component.text("»").color(NamedTextColor.GRAY)).append(Component.space())
-			.append(
-				SafeMiniMessage.mmDeserializeWithoutEvents(text))
-			.replaceText(URL_REPLACEMENT_CONFIG);
-	}
+        return component.append(Component.space()).append(
+                        PlexUtils.mmDeserialize(plex.config.getString("chat.name-color", "<white>") +
+                                MiniMessage.builder().tags(
+                                        TagResolver.resolver(StandardTags.color(), StandardTags.rainbow(),
+                                                StandardTags.decorations(), StandardTags.gradient(),
+                                                StandardTags.transition()
+                                        )).build().serialize(plexPlayer.getPlayer().displayName())))
+                .append(Component.space())
+                .append(Component.text("»").color(NamedTextColor.GRAY)).append(Component.space())
+                .append(
+                        SafeMiniMessage.mmDeserializeWithoutEvents(text))
+                .replaceText(URL_REPLACEMENT_CONFIG);
+    }
 
-	@Override
-	public void init(NushModule module) {
-		MODULE = NushModule.getInstance();
-	}
+    @Override
+    public void init(NushModule module)
+    {
+        MODULE = NushModule.getInstance();
+    }
 }
