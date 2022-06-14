741585c8dcdcd84bd023ac456dcaf58a7db2484e
Telesphoreo <me@telesphoreo.me>
2022-06-13 23:03:20 -0500

    Update to 1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19-R0.1-SNAPSHOT")
     compileOnly("dev.plex:server:1.2-SNAPSHOT")
     compileOnly("dev.plex:api:1.2-SNAPSHOT")
 }
