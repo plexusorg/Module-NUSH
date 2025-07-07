81033ed3ca7f93e8d133619c0ba89708ca77869a
Telesphoreo <me@telesphoreo.me>
2025-07-07 17:47:54 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,14 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
-    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    compileOnly("dev.plex:server:1.5-SNAPSHOT")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    compileOnly("io.papermc.paper:paper-api:1.21.7-R0.1-SNAPSHOT")
+    compileOnly("dev.plex:server:1.5")
 }
 
 group = "dev.plex"
-version = "1.5-SNAPSHOT"
+version = "1.5"
 description = "Module-NUSH"
 
 java {
